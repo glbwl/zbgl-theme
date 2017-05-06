2c2e49457e128d4316de5fd7bc57a87bd83c5016
glbwl <glbwl@qq.com>
2017-05-06 09:41:41 +0800

    主题文件

--- zbgl/template/footer.php
+++ zbgl/template/footer.php
@@ -1,4 +1,4 @@
-<?php header("Content-type: text/html;charset=utf-8");echo '出错了，不能这样。http://www.glbwl.com';exit();?>{* Template Name:公共底部 *}<div class="Blogfooter"><div class="CopyRight-center">Copyright &copy <a href="{$host}">{$name}</a>&nbsp; 版权所有<br/>{$copyright} 基于{$zblogphpabbrhtml}搭建<br/>主题名称：<a href="http://www.glbwl.com">zbgl 1.0</a></div></div><div class="top"></div></div><script type="text/javascript" src="{$host}zb_users/theme/{$theme}/script/zbgl.js"></script>
+<?php header("Content-type: text/html;charset=utf-8");echo '出错了，不能这样。http://www.glbwl.com';exit();?>{* Template Name:公共底部 *}<div class="Blogfooter"><div class="CopyRight-center">Copyright &copy <a href="{$host}">{$name}</a>&nbsp; 版权所有<br/>{$copyright} 基于{$zblogphpabbrhtml}搭建<br/>主题名称：<a href="http://www.glbwl.com">zbgl v1.2</a></div></div><div class="top"></div></div><script type="text/javascript" src="{$host}zb_users/theme/{$theme}/script/zbgl.js"></script>
 <script>
 (function(){
     var bp = document.createElement('script');
